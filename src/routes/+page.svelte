1b2edef5108f945bbf455e9723632ec989d61667
AndyLnd <1122618+AndyLnd@users.noreply.github.com>
2024-03-05 21:46:21 +0100

    Get images in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,11 @@
-<script>
-  import ArrowRightIcon from '$lib/components/ArrowRightIcon.svelte'
-  import PostsList from '$lib/components/PostsList.svelte'
-  import SocialLinks from '$lib/components/SocialLinks.svelte'
-  import { avatar, bio, name } from '$lib/info.js'
+<header>
+  <img src="/heathaze.webp" class="max-w-52" />
+</header>
 
-  /** @type {import('./$types').PageData} */
-  export let data
-</script>
+<div class="h-full flex justify-center items-start">
+  <img src="/laserknights.webp" class="max-w-64" />
+</div>
 
-<svelte:head>
-  <title>{name}</title>
-  <meta name="description" content={bio} />
-</svelte:head>
-
-<div class="flex flex-col flex-grow gap-8 pb-16">
-  <!-- bio -->
-  <section class="flex flex-col items-center gap-16 pt-8 pb-16">
-    <div class="flex flex-col items-center w-full gap-6 rounded-lg">
-      <img
-        src={avatar}
-        alt={name}
-        class="mx-auto rounded-full w-36 h-36 ring-2 ring-zinc-200 dark:ring-zinc-700"
-      />
-      <div class="flex gap-6">
-        <SocialLinks />
-      </div>
-      <p class="text-base text-zinc-600 dark:text-zinc-400">
-        {bio}
-      </p>
-    </div>
-  </section>
-  <section class="w-full">
-    <div class="flex items-center justify-between gap-4 mb-8">
-      <h2 class="text-sm font-medium sm:text-base text-zinc-500 dark:text-zinc-400">
-        Recently Published
-      </h2>
-      <a href="/posts" class="flex items-center gap-1 text-sm font-medium text-teal-500"
-        >View All <ArrowRightIcon class="w-4 h-4" /></a
-      >
-    </div>
-    <PostsList posts={data.posts} />
-  </section>
+<div class="fixed bottom-0 right-16 -z-10">
+  <img src="/zak.webp" class="max-w-80" />
 </div>
