ece4da89804313deb2ecd1c550c2c445e2ea45a3
AndyLnd <1122618+AndyLnd@users.noreply.github.com>
2024-03-06 23:29:57 +0100

    links to the right

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,24 +7,27 @@
 </svelte:head>
 
 <main class="flex flex-col flex-grow w-full mx-auto max-w-5xl h-full gap-16">
-  <header class="flex gap-10 items-center border-b border-white pb-8">
+  <header class="flex gap-10 items-center justify-between border-b border-white pb-8">
     <a href="/">
       <img src="/heathaze.webp" class="max-w-40 aspect-auto" alt="Heat Haze Games logo" /></a
     >
-    <a href="https://store.steampowered.com/app/2512850/Tales_of_the_Laser_Knights"
-      ><img src="/steam.svg" alt="Steam" class="w-10" /></a
-    >
-    <a href="https://www.tiktok.com/@realdenzi"
-      ><img src="/tiktok.svg" alt="TikTok" class="w-9" /></a
-    >
-    <a href="https://www.youtube.com/@heathazegames5701"
-      ><img src="/youtube.svg" alt="YouTube" class="w-11" /></a
-    >
-    <a href="https://www.instagram.com/heathazegames/"
-      ><img src="/instagram.svg" alt="Instagram" class="w-10" /></a
-    >
-    <a href="https://discord.gg/nvNMYtn5H4"><img src="/discord.svg" alt="Discord" class="w-9" /></a
-    >
+    <div class="flex gap-10 items-center">
+      <a href="https://store.steampowered.com/app/2512850/Tales_of_the_Laser_Knights"
+        ><img src="/steam.svg" alt="Steam" class="w-10" /></a
+      >
+      <a href="https://www.tiktok.com/@realdenzi"
+        ><img src="/tiktok.svg" alt="TikTok" class="w-9" /></a
+      >
+      <a href="https://www.youtube.com/@heathazegames5701"
+        ><img src="/youtube.svg" alt="YouTube" class="w-11" /></a
+      >
+      <a href="https://www.instagram.com/heathazegames/"
+        ><img src="/instagram.svg" alt="Instagram" class="w-10" /></a
+      >
+      <a href="https://discord.gg/nvNMYtn5H4"
+        ><img src="/discord.svg" alt="Discord" class="w-9" /></a
+      >
+    </div>
   </header>
   <slot />
 </main>
