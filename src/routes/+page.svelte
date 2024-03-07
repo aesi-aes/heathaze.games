9dfed43fce7fe9c0a9e2d4e77002cbd1c4f5ac28
AndyLnd <1122618+AndyLnd@users.noreply.github.com>
2024-03-07 23:30:43 +0100

    wishlist button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,10 +19,13 @@
 
   <a
     href="https://store.steampowered.com/app/2512850/Tales_of_the_Laser_Knights/"
-    class="group font-bold text-2xl drop-shadow-text text-center flex flex-col items-center"
+    class="rounded-lg flex font-bold text-2xl drop-shadow-text gap-4 items-center py-2 px-4 border shadow-md hover:shadow-none hover:translate-y-0.5 transition-all"
   >
-    Wishlist on Steam
-    <div class="bg-white h-0.5 w-0 group-hover:w-full transition-all" />
+    <img src="/steam.svg" class="w-14" />
+    <div class="text-left leading-[.9]">
+      <span>Wishlist on</span><br />
+      <span class="text-4xl">Steam</span>
+    </div>
   </a>
 </div>
 
